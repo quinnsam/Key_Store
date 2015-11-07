577ec189a5d55408c354691c53de7278b612819b
Sam Quinn <quinnsam1@gmail.com>
2015-11-07 11:20:53 -0800

    Added new activity to view keys store on the API and display them in a scrollable view.

--- app/src/main/java/com/oregonstate/squinn/key_store/MainActivity.java
+++ app/src/main/java/com/oregonstate/squinn/key_store/MainActivity.java
@@ -6,6 +6,7 @@ import android.app.ActionBar;
 import android.app.Fragment;
 import android.app.FragmentManager;
 import android.content.Context;
+import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.Gravity;
@@ -158,6 +159,11 @@ public class MainActivity extends Activity
             ((MainActivity) activity).onSectionAttached(
                     getArguments().getInt(ARG_SECTION_NUMBER));
         }
+
+    }
+    public void openViewActivity(View view) {
+        Intent intent = new Intent(MainActivity.this, ViewActivity.class);
+        startActivity(intent);
     }
 
 }
